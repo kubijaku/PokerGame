874855f5f5d2093246ddc0a944d6390d401d2e28
kubijaku <kubijaku11@gmail.com>
2022-11-24 20:12:54 +0100

    serverahmnndler

---
+++ PokerClient/src/main/java/pl/edu/agh/kis/pz1/ServerHandler.java
@@ -0,0 +1,41 @@
+package pl.edu.agh.kis.pz1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ServerHandler implements Runnable{
+
+    private Socket server;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public ServerHandler(Socket s) throws IOException {
+        server = s;
+        in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        out = new PrintWriter(server.getOutputStream(), true);
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true){
+                String serverResponse = in.readLine();
+                if ( serverResponse == null ) break;
+
+                System.out.println("Server says: " + serverResponse);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
